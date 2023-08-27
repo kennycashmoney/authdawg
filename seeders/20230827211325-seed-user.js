292fd6421c680deb5cf6def092d18fd26e67b1a9
kennycashmoney <ken.larose@proton.me>
2023-08-27 16:19:40 -0500

    initialized the db, migrated the User model, seeded the data

---
+++ seeders/20230827211325-seed-user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Users', [{
+    username: 'kennycashmoney',
+    password: 'asdf',
+    last_logon: new Date().toDateString(),
+    createdAt: new Date().toDateString(),
+    updatedAt: new Date().toDateString()
+   }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
