96e3b8a19126dd886496705a1ed20e393193b316
kennycashmoney <ken.larose@proton.me>
2023-08-27 17:33:43 -0500

    completed the authenticateUser endpoint

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./models');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT;
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-    // db.User.findAll()
-    //     .then((users) => res.send(users) )
-    //     .catch((error) => {
-    //         console.log(`db error... ${JSON.stringify(error)}`);
-    //         return res.send(error);
-    //     });
-    db.User.findByPk(2)
-        .then(user => user.destroy());
-        res.send(201);
-});
 
-//TODO: service to create a new user
+const dotenv = require('dotenv');
+
+// const port = process.env.PORT;
+const port = 3000;
+
+dotenv.config();
+app.use(bodyParser.json());
+
+/** 
+ * API endpoints
+ */
+
+// Service to create a new user
 app.post('/v1/createNewUser', async (req, res) => {
     const { username, password } = req.body;
+    // TODO: validate the username doesn't already exist
     db.User.create({ username, password })
         .then((user) => res.send(user))
         .catch((error) => {
@@ -30,8 +28,16 @@ app.post('/v1/createNewUser', async (req, res) => {
         });
 });
 
-//TODO: service to authenticate a user's credentials for log-in
-
+// Service to authenticate a user's credentials for log-in
+app.get('/v1/authenticateUser', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await db.User.findOne({ where: { username }});
+    if (user && user.password === password) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(401);
+    }
+});
 
 
 app.listen(port, () => {
