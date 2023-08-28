5bd3d692bc9d3544653d3f46fa5c180dd700ac4c
kennycashmoney <ken.larose@proton.me>
2023-08-28 13:48:05 -0500

    upon create new user req, check if username already exists

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const db = require('./models');
 const app = express();
 
 const dotenv = require('dotenv');
-
-// const port = process.env.PORT;
-const port = 3000;
-
 dotenv.config();
+const port = process.env.PORT;
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000'
+    // origin: 'https://productivitypal.vercel.app'
+}));
+
+
 app.use(bodyParser.json());
 
 /** 
@@ -19,19 +24,33 @@ app.use(bodyParser.json());
 
 // Service to create a new user
 app.post('/v1/createNewUser', async (req, res) => {
+
+    // TODO: figure out how to catch encrypted credentials
+    // console.log(req);
+    console.log('Request to create new user...');
+    // console.log(`Request body: ${JSON.stringify(req.body)}`);
+    // console.log(`Request header credentials: ${JSON.stringify(req.headers.authorization)}`);
     const { username, password } = req.body;
-    // TODO: validate the username doesn't already exist
-    db.User.create({ username, password })
-        .then((user) => res.send(user))
-        .catch((error) => {
-            res.status(500).send(error)
-        });
+
+    // validate the username doesn't already exist
+    const userExists = await db.User.findOne({ where: { username } });
+    if (!userExists) {
+        db.User.create({ username, password })
+            .then((user) => res.sendStatus(201))
+            .catch((error) => {
+                res.status(500).send(error);
+            });
+    } else {
+        res.status(400).send('Username already exists');
+    }
+
+
 });
 
 // Service to authenticate a user's credentials for log-in
 app.get('/v1/authenticateUser', async (req, res) => {
     const { username, password } = req.body;
-    const user = await db.User.findOne({ where: { username }});
+    const user = await db.User.findOne({ where: { username } });
     if (user && user.password === password) {
         res.sendStatus(200);
     } else {
